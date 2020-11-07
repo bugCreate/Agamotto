bab200331155f103074a8186537bfee1fa1aa747
qihaiming <qihaiming@sensorsdata.cn>
2020-11-07 17:55:44 +0800

    bug

--- platform/config-server/src/main/java/com/tang/platform/config/server/meta/repo/GroupDao.java
+++ platform/config-server/src/main/java/com/tang/platform/config/server/meta/repo/GroupDao.java
@@ -8,27 +8,27 @@ import java.util.List;
 
 @Mapper
 public interface GroupDao {
-    @Select("select id,name,comment,group_type from group where id =#{id}")
+    @Select("select id,name,comment,group_type from config_group where id =#{id}")
     @Results(id = "group", value = {
             @Result(property = "groupType", column = "group_type")
     })
     Group findGroupById(@Param("id") int id);
 
-    @Select("select id,name,comment,group_type from group where name =#{name}")
+    @Select("select id,name,comment,group_type from config_group where name =#{name}")
     @ResultMap(value = "group")
     Group findGroupByName(@Param("name") String name);
 
-    @Insert("insert into group values(#{name},#{comment},#{groupType})")
+    @Insert("insert into config_group(name,comment,group_type) values(#{name},#{comment},#{groupType})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     void create(Group group);
 
-    @Update("update group set name=#{name},comment=#{comment},group_type =#{groupType}")
+    @Update("update config_group set name=#{name},comment=#{comment},group_type =#{groupType}")
     void update(Group group);
 
-    @Delete("delete from group where id=#{id}")
+    @Delete("delete from config_group where id=#{id}")
     void delete(@Param("id") int id);
 
-    @Select("select id,name,comment,group_type from group")
+    @Select("select id,name,comment,group_type from config_group")
     @ResultMap(value = "group")
     List<Group> findAll();
 }
